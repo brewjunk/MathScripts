9b5d8a7b48abfba7711a1ae49a950555b0212aaa
brewjunk <tom.b@vicioustech.com>
2022-06-12 18:37:06 +1200

    Script renamed to more appropriate filename.

--- circle_dimensions.py
+++ circle_dimensions.py
@@ -1,12 +1,64 @@
 import math
-def triangle(base, height):
-    return base*height/2
+while True:
+    print("""
+          |||||||||||!!||||||||||||||!!||||||||||||||!!||||||||||||||!!|||
+          |||||||||||!!||||||||||||||!!||||||||||||||!!||||||||||||||!!|||
+          ||||||!!||Find Circumference,radius,area of circle.||||||||!!|||
+          |||||||||||!!||||||||||||||!!||||||||||||||!!||||||||||||||!!|||
+          C = 2πr
+                A = πr2
+                A = π × r2, where 'r' is the radius.
+                A = (π/4) × d2, where 'd' is the diameter.
+                A = C2/4π, where 'C' is the circumference.
+                    r = C / π/2
+                        D = r * 2
 
-def rectangle(base, height):
-    return base*height
+    """)
+    def find_circumference(r):
+        """C=2πr"""
+        C = 2 * (math.pi) * float(r)
+        print("          The circumference of the circle is:{:0.4f} units".format(C))
 
-def circle(radius):
-    return math.pi*(radius**2)
+    def find_area_of_circle(r):
+        """A = πr2
+        Area = π × r2, where 'r' is the radius.
+        Area = (π/4) × d2, where 'd' is the diameter.
+        Area = C2/4π, where 'C' is the circumference."""
+        A = (math.pi) * (float(r)*float(r))
+        print("          The area of the circle is :{:0.4f} Squared units".format(A))
 
-def donut(outside_radius, inside_radius):
-    return circle(outside_radius) - circle(inside_radius)
+    def find_radius_of_circle(C):
+        """Find radius if you have circumference"""
+        r = float(C)/(math.pi)/2
+        print("          The radius of the circle is:{:0.4f} units".format(r))
+
+    # Ask user what they want to know!
+    ask = input("""          What would you like to find out ?
+                    [0] Circumference [1] Area of the circle [2] Radius""")
+    print("""
+
+    """)
+    if int(ask) == 0:
+        find_circumference(input("          Please enter the radius of the circle to find circumference:"))
+    elif int(ask) == 1:
+        find_area_of_circle(input("          Please enter the radius of the circle to find area:"))
+    elif int(ask) == 2:
+        find_radius_of_circle(input("          Please enter the circumference of the circle to find radius:"))
+    # Ask user what they want to know!
+    print("""
+
+    """)
+    check = input("""          Would you like to find another?:
+          (Type 1 to try another, Or 0 to quit!)
+        """)
+    print("""
+
+          |||||||||||!!||||||||||||||!!||||||||||||||!!||||||||||||||!!|||""")
+    if check == "0":
+        print("          Ok then, Thanks! Good bye!")
+        print("""
+
+          |||||||||||!!||||||||||||||!!||||||||||||||!!||||||||||||||!!|||""")
+        break
+    elif check == "1":
+        continue
